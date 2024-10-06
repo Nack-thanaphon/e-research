e1b8161c3d802dcd2832de118c1efeaa3ba33b50
thanaphon <e21bvz@gmail.com>
2024-10-06 12:31:43 +0700

    fixed

--- include/config_db.php
+++ include/config_db.php
@@ -1,35 +1,39 @@
-<?
+<?php
 $hostname = "localhost";   
 $user = 'aree';
 $passwd = 'Ak@072039';
 $dbname = "buu_chanthaburi";
+
+// Enable error reporting for mysqli
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $mysqli = new mysqli($hostname, $user, $passwd, $dbname);
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') '
-            . $mysqli->connect_error);
-}
 $mysqli->set_charset('utf8');
 
-$sql = "select * from `ers_configs` where (`id`='1')";
-$dbquery = $mysqli->query($sql);
-$tRows = $dbquery->num_rows;
-if($tRows>0){
-	$row = $dbquery->fetch_assoc();
-	define('__EC_NAME__',$row["ec_name"]);
-	define('__EC_FAVICON_ICO__',$row["ec_favicon_ico"]);
-	define('__EC_FAVICON__',$row["ec_favicon"]);
-	define('__EC_LOGO__',$row["ec_logo"]);
-	define('__EC_PICHOME__',$row["ec_pichome"]);
-	define('__EC_MANUAL_ADMIN__',$row["ec_manual_admin"]);
-	define('__EC_MANUAL_MEMBER__',$row["ec_manual_member"]);
-	define('__EC_MANUAL_GUEST__',$row["ec_manual_guest"]);
-	define('__EC_COPYRIGHT__',$row["ec_copyright"]);
-	if(!empty($row["ec_bgshow"])){
-		define('__EC_BGSHOW__',$row["ec_bgshow"]);
-	} else { define('__EC_BGSHOW__','cc0066'); }
-	if(!empty($row["ec_fontshow"])){
-		define('__EC_FONTSHOW__',$row["ec_fontshow"]);
-	} else { define('__EC_FONTSHOW__','ffffff'); }
+$sql = "SELECT * FROM `ers_configs` WHERE `id` = ?";
+$stmt = $mysqli->prepare($sql);
+$id = 1;
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+$tRows = $result->num_rows;
+
+if ($tRows > 0) {
+    $row = $result->fetch_assoc();
+    define('__EC_NAME__', $row["ec_name"]);
+    define('__EC_FAVICON_ICO__', $row["ec_favicon_ico"]);
+    define('__EC_FAVICON__', $row["ec_favicon"]);
+    define('__EC_LOGO__', $row["ec_logo"]);
+    define('__EC_PICHOME__', $row["ec_pichome"]);
+    define('__EC_MANUAL_ADMIN__', $row["ec_manual_admin"]);
+    define('__EC_MANUAL_MEMBER__', $row["ec_manual_member"]);
+    define('__EC_MANUAL_GUEST__', $row["ec_manual_guest"]);
+    define('__EC_COPYRIGHT__', $row["ec_copyright"]);
+    define('__EC_BGSHOW__', !empty($row["ec_bgshow"]) ? $row["ec_bgshow"] : 'cc0066');
+    define('__EC_FONTSHOW__', !empty($row["ec_fontshow"]) ? $row["ec_fontshow"] : 'ffffff');
 }
-$dbquery->free();
+
+$result->free();
+$stmt->close();
+$mysqli->close();
 ?>
