ee46610f462443d0036b2f8374e5ea24456a1c59
thanaphon <e21bvz@gmail.com>
2024-10-23 09:40:57 +0700

    fixed

--- include/config.php
+++ include/config.php
@@ -10,8 +10,8 @@
 // 	return $httpURL;
 // }
 // $_https = sslHttp();
-// $wRoot = $_https . $_SERVER["SERVER_NAME"] . "/~scia/e-research";
-// $sRoot = $_SERVER["DOCUMENT_ROOT"] . "/e-research";
+$wRoot = $_SERVER["SERVER_NAME"] . "/~scia/e-research";
+$sRoot = $_SERVER["DOCUMENT_ROOT"] . "/e-research";
 define('_web_path', $wRoot, true);
 define('_home_path', $sRoot, true);
 define('SECRET_KEY', 'buu_@_e-research', true);
