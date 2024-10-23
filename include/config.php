06d263f1e38e612b405011b9ed68ba7fbff1274b
thanaphon <e21bvz@gmail.com>
2024-10-23 09:39:30 +0700

    fixed

--- include/config.php
+++ include/config.php
@@ -1,17 +1,17 @@
 <?php
 
-function sslHttp()
-{
-	$httpURL = 'http';
-	if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] != 'off')) {
-		$httpURL .= "s";
-	}
-	$httpURL .= "://";
-	return $httpURL;
-}
-$_https = sslHttp();
-$wRoot = $_https . $_SERVER["SERVER_NAME"] . "/~scia/e-research";
-$sRoot = $_SERVER["DOCUMENT_ROOT"] . "/e-research";
-define('_web_path', $wRoot, true);
-define('_home_path', $sRoot, true);
-define('SECRET_KEY', 'buu_@_e-research', true);
+// function sslHttp()
+// {
+// 	$httpURL = 'http';
+// 	if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] != 'off')) {
+// 		$httpURL .= "s";
+// 	}
+// 	$httpURL .= "://";
+// 	return $httpURL;
+// }
+// $_https = sslHttp();
+// $wRoot = $_https . $_SERVER["SERVER_NAME"] . "/~scia/e-research";
+// $sRoot = $_SERVER["DOCUMENT_ROOT"] . "/e-research";
+// define('_web_path', $wRoot, true);
+// define('_home_path', $sRoot, true);
+// define('SECRET_KEY', 'buu_@_e-research', true);
