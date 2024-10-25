b2819cba4bbbd73d97119f5c527f54fcc78d4602
thanaphon <e21bvz@gmail.com>
2024-10-26 01:23:46 +0700

    fixed

--- admin/logout.php
+++ admin/logout.php
@@ -3,23 +3,23 @@ session_start();
 Header("Content-Type: text/html; charset=UTF-8");
 require_once "../include/config.php";
 if (!defined('_web_path')) {
-	exit;
+    exit;
 }
-if ( isset($_SESSION["admin"]) && isset($_SESSION["userlevel"]) )
-{
-	include("../include/config_db.php");
-	$admin = $_SESSION["admin"];
-	$u_ip = $_SERVER["REMOTE_ADDR"];
-	//$now = date("Y-m-d H:i:s",time());
-	$query_m = "insert into ers_session  (id,user_name,ip_address,log_time,log_status) values ('','$admin','$u_ip',now(),'o') ";
-	$result_d = $mysqli->query($query_m);
-	include("../include/close_db.php");
+if (isset($_SESSION["admin"]) && isset($_SESSION["userlevel"])) {
+    include("../include/config_db.php");
+    $admin = $_SESSION["admin"];
+    $u_ip = $_SERVER["REMOTE_ADDR"];
+    //$now = date("Y-m-d H:i:s",time());
+    $query_m = "INSERT INTO ers_session (user_name, ip_address, log_time, log_status) VALUES ('$admin', '$u_ip', NOW(), 'o')";
+    $result_d = $mysqli->query($query_m);
+    include("../include/close_db.php");
 }
 
 unset($_SESSION["admin"]);
 unset($_SESSION["userlevel"]);
 unset($_SESSION["userid"]);
-session_unset();session_destroy();
+session_unset();
+session_destroy();
 
 //echo "<br><br><p align=\"center\"><font face=\"ms sans serif\" size=4 color=red><b>...ออกจากระบบ...</b></font></p>";	  
 
