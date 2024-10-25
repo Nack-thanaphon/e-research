330659486509d772ad151060d8d89ea8eeb3c0cb
thanaphon <e21bvz@gmail.com>
2024-10-26 01:14:16 +0700

    fixed

--- include/config.php
+++ include/config.php
@@ -16,11 +16,3 @@ define('_web_path', '/~scia/e-research');
 define('_home_path', '/~scia/e-research');
 define('SECRET_KEY', 'buu_@_e-research');
 
-function dateThai_edo($strDate) {
-    $strYear = date("Y",strtotime($strDate))+543;
-    $strMonth= date("n",strtotime($strDate));
-    $strDay= date("j",strtotime($strDate));
-    $strMonthCut = Array("","มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม");
-    $strMonthThai=$strMonthCut[$strMonth];
-    return "$strDay $strMonthThai $strYear";
-}
--- include/convars.php
+++ include/convars.php
@@ -117,4 +117,13 @@ class Paginator {
             return mb_substr($str, $start, $length);
         }
     }
+
+    public function dateThai_edo($strDate) {
+        $strYear = date("Y",strtotime($strDate))+543;
+        $strMonth= date("n",strtotime($strDate));
+        $strDay= date("j",strtotime($strDate));
+        $strMonthCut = Array("","มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม");
+        $strMonthThai=$strMonthCut[$strMonth];
+        return "$strDay $strMonthThai $strYear";
+    }
 }
